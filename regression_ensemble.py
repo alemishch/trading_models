da7cc3d6108a897c04ae2d43d7372efe7c9b5675
alemishch <mishchenko.ad@phystech.edu>
2024-12-18 10:36:57 +0300

    fixed plots

--- regression_ensemble.py
+++ regression_ensemble.py
@@ -89,6 +89,10 @@ strategies = [
     "G73",
     "G43",
     "G59_V1",
+    "G69_V2",
+    "G90_V1",
+    "G53_V1",
+    "G19_V5",
 ]
 
 feature_sets = {"With_MF": True, "Without_MF": False}
@@ -157,118 +161,108 @@ def leave_important(X_train, y_train, X_test, n):
     return X_train_selected, X_test_selected, importances
 
 
-backup_dir = "regression_backup"
-os.makedirs(backup_dir, exist_ok=True)
-
-for strategy in tqdm(strategies):
-    df = merged_data[strategy].copy()
-    df = df.sort_index()
-
-    for target in tqdm(targets):
-        for model_name, model in models.items():
-            for fs_name, include_market in feature_sets.items():
-                feature_cols = get_feature_columns(df, include_market)
-                current_train_end = pd.to_datetime(train_end)
-                current_predict_start = pd.to_datetime(predict_start)
-                current_predict_end = pd.to_datetime(predict_end)
-
-                while current_predict_start <= df.index.max():
-                    train = df.loc[:current_train_end]
-                    predict = df.loc[current_predict_start:current_predict_end]
-
-                    X_train = train[feature_cols]
-                    y_train = train[target]
-
-                    X_predict = predict[feature_cols]
-                    y_true = predict[target]
-
-                    if len(X_train) == 0:
-                        current_train_end = current_predict_end
-                        current_predict_start = current_train_end + pd.Timedelta(days=1)
-                        current_predict_end = (
-                            current_predict_start + step - pd.Timedelta(days=1)
-                        )
-                        if current_predict_end > df.index.max():
-                            current_predict_end = df.index.max()
-                        continue
-
-                    try:
-                        (
-                            X_train_selected,
-                            X_predict_selected,
-                            importances,
-                        ) = leave_important(X_train, y_train, X_predict, n=15)
-                    except Exception as e:
-                        print(e)
-                        current_train_end = current_predict_end
-                        current_predict_start = current_train_end + pd.Timedelta(days=1)
-                        current_predict_end = (
-                            current_predict_start + step - pd.Timedelta(days=1)
-                        )
-                        if current_predict_end > df.index.max():
-                            current_predict_end = df.index.max()
-                        continue
-
-                    try:
-                        model.fit(X_train_selected, y_train)
-
-                        try:
-                            y_pred = model.predict(X_predict_selected)
-                            y_pred_shifted = (
-                                pd.Series(y_pred, index=predict.index).shift().fillna(1)
-                            )
-                            strategy_model_dfs[strategy][model_name][fs_name].loc[
-                                y_pred_shifted.index, f"Preds_{model_name}_{target}"
-                            ] = y_pred_shifted
-                        except Exception as e:
-                            print(
-                                f"Error for {strategy}, Model: {model_name}, Target: {target}, Feature Set: {fs_name}. Error: {e}"
-                            )
-                            # Fill with 1
-                            strategy_model_dfs[strategy][model_name][fs_name].loc[
-                                predict.index, f"Preds_{model_name}_{target}"
-                            ] = 0
-
-                    except Exception as e:
-                        print(e)
-                        current_train_end = current_predict_end
-                        current_predict_start = current_train_end + pd.Timedelta(days=1)
-                        current_predict_end = (
-                            current_predict_start + step - pd.Timedelta(days=1)
-                        )
-                        if current_predict_end > df.index.max():
-                            current_predict_end = df.index.max()
-                        continue
-
-                    current_train_end = current_predict_end
-                    current_predict_start = current_train_end + pd.Timedelta(days=1)
-                    current_predict_end = (
-                        current_predict_start + step - pd.Timedelta(days=1)
-                    )
-
-                    if current_predict_end > df.index.max():
-                        current_predict_end = df.index.max()
-
-                file_name = f"{strategy}_{model_name}_{fs_name}.xlsx"
-                file_path = os.path.join(backup_dir, file_name)
-                try:
-                    strategy_model_dfs[strategy][model_name][fs_name].to_excel(
-                        file_path, index=True
-                    )
-                    print(f"Saved {file_name} successfully.")
-                except Exception as e:
-                    print(f"Error saving {file_name}: {e}")
-
-
-def calculate_sharpe_ratio(returns):
-    mean_return = returns.mean()
-    std_return = returns.std()
-    if std_return == 0:
-        return 0
-    return mean_return / std_return * np.sqrt(365)
-
-
 # backup_dir = "regression_backup"
+# os.makedirs(backup_dir, exist_ok=True)
+
+# for strategy in tqdm(strategies):
+#     df = merged_data[strategy].copy()
+#     df = df.sort_index()
+
+#     for target in tqdm(targets):
+#         for model_name, model in models.items():
+#             for fs_name, include_market in feature_sets.items():
+#                 feature_cols = get_feature_columns(df, include_market)
+#                 current_train_end = pd.to_datetime(train_end)
+#                 current_predict_start = pd.to_datetime(predict_start)
+#                 current_predict_end = pd.to_datetime(predict_end)
+
+#                 while current_predict_start <= df.index.max():
+#                     train = df.loc[:current_train_end]
+#                     predict = df.loc[current_predict_start:current_predict_end]
+
+#                     X_train = train[feature_cols]
+#                     y_train = train[target]
+
+#                     X_predict = predict[feature_cols]
+#                     y_true = predict[target]
+
+#                     if len(X_train) == 0:
+#                         current_train_end = current_predict_end
+#                         current_predict_start = current_train_end + pd.Timedelta(days=1)
+#                         current_predict_end = (
+#                             current_predict_start + step - pd.Timedelta(days=1)
+#                         )
+#                         if current_predict_end > df.index.max():
+#                             current_predict_end = df.index.max()
+#                         continue
+
+#                     try:
+#                         (
+#                             X_train_selected,
+#                             X_predict_selected,
+#                             importances,
+#                         ) = leave_important(X_train, y_train, X_predict, n=15)
+#                     except Exception as e:
+#                         print(e)
+#                         current_train_end = current_predict_end
+#                         current_predict_start = current_train_end + pd.Timedelta(days=1)
+#                         current_predict_end = (
+#                             current_predict_start + step - pd.Timedelta(days=1)
+#                         )
+#                         if current_predict_end > df.index.max():
+#                             current_predict_end = df.index.max()
+#                         continue
+
+#                     try:
+#                         model.fit(X_train_selected, y_train)
+
+#                         try:
+#                             y_pred = model.predict(X_predict_selected)
+#                             y_pred_shifted = (
+#                                 pd.Series(y_pred, index=predict.index).shift().fillna(1)
+#                             )
+#                             strategy_model_dfs[strategy][model_name][fs_name].loc[
+#                                 y_pred_shifted.index, f"Preds_{model_name}_{target}"
+#                             ] = y_pred_shifted
+#                         except Exception as e:
+#                             print(
+#                                 f"Error for {strategy}, Model: {model_name}, Target: {target}, Feature Set: {fs_name}. Error: {e}"
+#                             )
+#                             # Fill with 1
+#                             strategy_model_dfs[strategy][model_name][fs_name].loc[
+#                                 predict.index, f"Preds_{model_name}_{target}"
+#                             ] = 0
+
+#                     except Exception as e:
+#                         print(e)
+#                         current_train_end = current_predict_end
+#                         current_predict_start = current_train_end + pd.Timedelta(days=1)
+#                         current_predict_end = (
+#                             current_predict_start + step - pd.Timedelta(days=1)
+#                         )
+#                         if current_predict_end > df.index.max():
+#                             current_predict_end = df.index.max()
+#                         continue
+
+#                     current_train_end = current_predict_end
+#                     current_predict_start = current_train_end + pd.Timedelta(days=1)
+#                     current_predict_end = (
+#                         current_predict_start + step - pd.Timedelta(days=1)
+#                     )
+
+#                     if current_predict_end > df.index.max():
+#                         current_predict_end = df.index.max()
+
+
+# def calculate_sharpe_ratio(returns):
+#     mean_return = returns.mean()
+#     std_return = returns.std()
+#     if std_return == 0:
+#         return 0
+#     return mean_return / std_return * np.sqrt(365)
+
+
+backup_dir = "regression_backup"
 # os.makedirs(backup_dir, exist_ok=True)
 
 # for strategy, models_dict in strategy_model_dfs.items():
@@ -285,11 +279,52 @@ def calculate_sharpe_ratio(returns):
 
 # print(strategy_model_dfs["G19_V5"]["CatBoost"]["With_Market_Features"].head(100))
 # print(strategy_model_dfs["G19_V5"]["LightGBM"]["With_Market_Features"].head(100))
+
+
+strategy_model_dfs = defaultdict(lambda: defaultdict(dict))
+mf = {"With_Market_Features: With_MF, Without_Market_Features": "Without_MF"}
+
+
+def load_data_to_dict(directory):
+    for file_name in os.listdir(directory):
+        if file_name.endswith(".xlsx"):
+            try:
+                file_parts = file_name.replace(".xlsx", "").split("_")
+                if len(file_parts) == 4:
+                    strategy = file_parts[0]
+                    model_name = file_parts[1]
+                    fs_name = "_".join(file_parts[2:])
+
+                    file_path = os.path.join(directory, file_name)
+
+                    df = pd.read_excel(file_path, index_col=0)
+                    print(strategy, model_name, fs_name)
+                    strategy_model_dfs[strategy][model_name][fs_name] = df
+                    print(f"Loaded {file_name}")
+                elif len(file_parts) >= 5:
+                    strategy = f"{file_parts[0]}_{file_parts[1]}"
+                    model_name = file_parts[2]
+                    fs_name = "_".join(file_parts[3:])
+
+                    file_path = os.path.join(directory, file_name)
+
+                    df = pd.read_excel(file_path, index_col=0)
+                    print(strategy, model_name, fs_name)
+                    strategy_model_dfs[strategy][model_name][fs_name] = df
+                    print(f"Loaded {file_name}")
+                else:
+                    print(f" invalid file name {file_name}")
+            except Exception as e:
+                print(f"Error loading {file_name}: {e}")
+    return strategy_model_dfs
+
+
+strategy_model_dfs = load_data_to_dict(backup_dir)
 for strategy in strategies:
     for target in targets:
         plt.figure(figsize=(10, 6))
         for i, model in enumerate(models):
-            for features in feature_sets:
+            for j, features in enumerate(feature_sets):
                 df = strategy_model_dfs[strategy][model][features].copy()
                 if strategy in ["G59_V1", "G59_V2", "G90_V1", "G24", "G58_V1"]:
                     test_period = df.loc["2024"]
@@ -303,11 +338,15 @@ for strategy in strategies:
                 pred_zero_abs = test_period.loc[
                     test_period[f"Preds_{model}_{target}"] < 0
                 ].index
-                pred_zero_cur = df.loc[
-                    (df.index >= start)
-                    & (df.index <= "2024-12-31")
-                    & (df[f"Preds_{model}_{target}"] < df[trues[target]])
-                ].index
+
+                if target in ["Sharpe_Target_10", "Sharpe_Target_30"]:
+                    shifted_preds = df[f"Preds_{model}_{target}"].shift(-1).fillna(1)
+                    pred_zero_cur = df.loc[
+                        (df.index >= start)
+                        & (df.index <= "2024-12-31")
+                        & (shifted_preds < df[trues[target]])
+                    ].index
+                    pred_zero_cur = pred_zero_cur.shift(1, freq="D").dropna()
 
                 sim_returns_abs = test_period["Mean_Returns_1"].copy()
                 sim_returns_abs.loc[test_period.index.isin(pred_zero_abs)] = 0
@@ -317,7 +356,7 @@ for strategy in strategies:
                 sim_returns_cur.loc[test_period.index.isin(pred_zero_cur)] = 0
                 cumulative_sim_cur = sim_returns_cur.cumsum()
 
-                if i == 0:
+                if i == 0 and j == 0:
                     plt.plot(
                         test_period.index,
                         cumulative_returns,
@@ -328,15 +367,17 @@ for strategy in strategies:
                 plt.plot(
                     test_period.index,
                     cumulative_sim_abs,
-                    label=f"Pred {target} by {model} {features} (current)",
-                    linestyle=":",
-                )
-                plt.plot(
-                    test_period.index,
-                    cumulative_sim_cur,
                     label=f"Pred {target} by {model} {features} (abs)",
                     linestyle=":",
                 )
+
+                if target in ["Sharpe_Target_10", "Sharpe_Target_30"]:
+                    plt.plot(
+                        test_period.index,
+                        cumulative_sim_cur,
+                        label=f"Pred {target} by {model} {features} (current)",
+                        linestyle=":",
+                    )
 
         plt.title(f"{strategy} - regression")
         plt.ylabel(f"Сг")
@@ -347,51 +388,51 @@ for strategy in strategies:
         plt.savefig(f"graph/regression/{strategy}_{target}.png")
         # plt.show()
 
-# simulation_results = []
-# end_date = "2024-12-31"
+simulation_results = []
+end_date = "2024-12-31"
 
-# for strategy in strategies:
-#     for model_name in models.keys():
-#         for feature_set, include_market in feature_sets.items():
-#             for target in targets:
-#                 try:
-#                     if strategy in ["G59_V1", "G59_V2", "G90_V1", "G24", "G58_V1"]:
-#                         start_date = "2024-01-01"
-#                     else:
-#                         start_date = "2024-07-01"
+for strategy in strategies:
+    for model_name in models.keys():
+        for feature_set, include_market in feature_sets.items():
+            for target in targets:
+                try:
+                    if strategy in ["G59_V1", "G59_V2", "G90_V1", "G24", "G58_V1"]:
+                        start_date = "2024-01-01"
+                    else:
+                        start_date = "2024-07-01"
 
-#                     df = strategy_model_dfs[strategy][model_name][feature_set].copy()
+                    df = strategy_model_dfs[strategy][model_name][feature_set].copy()
 
-#                     df_sim = df.loc[start_date:end_date].copy()
+                    df_sim = df.loc[start_date:end_date].copy()
 
-#                     pred_column = f"Preds_{model_name}_{target}"
+                    pred_column = f"Preds_{model_name}_{target}"
 
-#                     if pred_column not in df_sim.columns:
-#                         print(
-#                             f"Prediction column {pred_column} not found in strategy {strategy}. Skipping."
-#                         )
-#                         continue
+                    if pred_column not in df_sim.columns:
+                        print(
+                            f"Prediction column {pred_column} not found in strategy {strategy}. Skipping."
+                        )
+                        continue
 
-#                     sim_returns = df_sim["Mean_Returns_1"].copy()
-#                     sim_returns[df_sim[pred_column] == 0] = 0
+                    sim_returns = df_sim["Mean_Returns_1"].copy()
+                    sim_returns[df_sim[pred_column] == 0] = 0
 
-#                     cumulative_sim = sim_returns.sum()
+                    cumulative_sim = sim_returns.sum()
 
-#                     simulation_results.append(
-#                         {
-#                             "Model": model_name,
-#                             "Feature_Set": feature_set,
-#                             "Target": target,
-#                             "Strategy": strategy,
-#                             "Cumulative_Returns": cumulative_sim,
-#                         }
-#                     )
+                    simulation_results.append(
+                        {
+                            "Model": model_name,
+                            "Feature_Set": feature_set,
+                            "Target": target,
+                            "Strategy": strategy,
+                            "Cumulative_Returns": cumulative_sim,
+                        }
+                    )
 
-#                 except Exception as e:
-#                     print(
-#                         f"Simulation error for Strategy: {strategy}, Model: {model_name}, Feature Set: {feature_set}, Target: {target}. Error: {e}"
-#                     )
-#                     continue
+                except Exception as e:
+                    print(
+                        f"Simulation error for Strategy: {strategy}, Model: {model_name}, Feature Set: {feature_set}, Target: {target}. Error: {e}"
+                    )
+                    continue
 
 
 # benchmark_results = []
